2ee1487c0e56576f3f341ce56a1e098716ecea2a
Dev8271 <155649280+Dev8271@users.noreply.github.com>
2024-10-15 01:41:19 +0530

    Create fullscreen.js

---
+++ fullscreen.js
@@ -0,0 +1,14 @@
+// fullscreen.js
+var iframe = document.getElementById("gameIframe");
+
+function openFullscreen() {
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari, Opera
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+        iframe.msRequestFullscreen();
+    }
+}
